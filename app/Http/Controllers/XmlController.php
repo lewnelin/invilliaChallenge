5a9f3945b2e9cbbd39e703301248aa81aa3808ed
arthur <arthur.ifpb@gmail.com>
2020-12-12 12:20:52 -0300

    Adicionado tratamento para order

--- app/Http/Controllers/XmlController.php
+++ app/Http/Controllers/XmlController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\Item;
 use App\Models\Order;
 use App\Models\Person;
 use App\Models\Phone;
@@ -18,49 +20,75 @@ class XmlController extends Controller
 
     /**
      * @param SimpleXMLElement $orderXml
-     * @return int
+     * @param array $processed
+     * @return array
      */
-    private static function processOrderXml(SimpleXMLElement $orderXml)
+    private static function processOrderXml(SimpleXMLElement $orderXml, array $processed)
     {
-        $order = new Order([
-            'id' => (int)$orderXml->orderid,
-            'person_id' => (int)$orderXml->orderperson
-        ]);
+        try {
+            $order = new Order([
+                'id' => (int)$orderXml->orderid,
+                'person_id' => (int)$orderXml->orderperson
+            ]);
 
-        $order->save();
+            $order->save();
 
-//        foreach ($orderXml->phones->phone as $phoneXml) {
-//            $phone = new Phone([
-//                'number' => (string)$phoneXml[0]
-//            ]);
-//
-//            $order->phones()->save($phone);
-//        }
+            $addressXml = $orderXml->shipto;
+            $address = new Address([
+                'name' => $addressXml->name,
+                'address' => $addressXml->address,
+                'city' => $addressXml->city,
+                'country' => $addressXml->country,
+            ]);
+            $order->address()->save($address);
 
-        return $order->id;
+            foreach ($orderXml->items->item as $itemXml) {
+                $item = new Item([
+                    'title' => (string)$itemXml->title,
+                    'note' => (string)$itemXml->note,
+                    'quantity' => (string)$itemXml->quantity,
+                    'price' => (string)$itemXml->price,
+                ]);
+
+                $order->items()->save($item);
+            }
+
+            $processed[] = $order->id;
+        } catch (\Exception $exception) {
+            $processed[] = 'An Error Occurred with id ' . $orderXml->orderid;
+        }
+
+        return $processed;
     }
 
     /**
      * @param SimpleXMLElement $personXml
-     * @return int
+     * @param array $processed
+     * @return array
      */
-    private static function processPeopleXml(SimpleXMLElement $personXml)
+    private static function processPeopleXml(SimpleXMLElement $personXml, array $processed)
     {
-        $person = new Person([
-            'id' => (int)$personXml->personid,
-            'name' => (string)$personXml->personname,
-        ]);
-        $person->save();
-
-        foreach ($personXml->phones->phone as $phoneXml) {
-            $phone = new Phone([
-                'number' => (string)$phoneXml[0]
+        try {
+            $person = new Person([
+                'id' => (int)$personXml->personid,
+                'name' => (string)$personXml->personname,
             ]);
+            $person->save();
 
-            $person->phones()->save($phone);
+            foreach ($personXml->phones->phone as $phoneXml) {
+                $phone = new Phone([
+                    'number' => (string)$phoneXml[0]
+                ]);
+
+                $person->phones()->save($phone);
+            }
+
+            $processed[] = $person->id;
+        } catch (\Exception $exception) {
+            $processed[] = 'An Error Occurred with id ' . $personXml->personid;
         }
 
-        return $person->id;
+        return $processed;
     }
 
     /**
@@ -81,12 +109,10 @@ class XmlController extends Controller
             foreach ($xml as $key => $elements) {
                 switch ($key) {
                     case 'person':
-                        $id = self::processPeopleXml($elements);
-                        $processed[] = $id;
+                        $processed = self::processPeopleXml($elements, $processed);
                         break;
                     case 'shiporder':
-                        $id = self::processOrderXml($elements);
-                        $processed[] = $id;
+                        $processed = self::processOrderXml($elements, $processed);
                         break;
                 }
             }
